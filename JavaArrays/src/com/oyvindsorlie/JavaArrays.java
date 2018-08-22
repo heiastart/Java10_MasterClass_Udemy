b1b6c0c6b466b41b5a584d7df14a1d8d4aaa36de
Oyvind Sorlie <heiastart@outlook.com>
2018-08-22 20:18:08 +0200

    Sorting arrays

--- JavaArrays/src/com/oyvindsorlie/JavaArrays.java
+++ JavaArrays/src/com/oyvindsorlie/JavaArrays.java
@@ -1,5 +1,7 @@
 package com.oyvindsorlie;
 
+import java.util.Arrays;
+
 public class JavaArrays {
 
     public static void main(String[] args) {
@@ -11,6 +13,7 @@ public class JavaArrays {
         // Alternative way to create an array, which is empty at first (MUST specify size!!);
         int[] newArray = new int[5];
         String[] newStringArray = new String[5];
+        newStringArray = new String[]{"Ole", "Per", "Frans", "Hans", "Knut", "Petter"};
 
         // Add stuff to the empty array;
         newArray[0] = 3;
@@ -19,14 +22,29 @@ public class JavaArrays {
         newArray[3] = 7;
         newArray[4] = 1;
 
-        //System.out.println(arrInts.length);
-
-        for (int i=0; i<arrInts.length; i++){
-            System.out.println("Item #" + (i + 1) + " = " + arrInts[i]);
+        for (int i=0; i<newStringArray.length; i++){
+            System.out.println(newStringArray[i]);
         }
 
+        // How to sort the content of an array
+        Arrays.sort(newStringArray);
+        //System.out.println(arrInts.length);
+        System.out.println("---------------------------");
+        System.out.println("Så sortert;");
+
+        for (String navn : newStringArray){
+            System.out.println(navn);
+        }
+        System.out.println();
+        System.out.println("Og for tall;");
+
+        for (int i=0; i<newArray.length; i++){
+            System.out.println("Tallet er: " + newArray[i]);
+        }
+        System.out.println("---------------------------");
+        Arrays.sort(newArray);
         // or like this;
-        for (int tall : arrInts){
+        for (int tall : newArray){
             System.out.println("Tallet er: " + tall);
         }
     }
